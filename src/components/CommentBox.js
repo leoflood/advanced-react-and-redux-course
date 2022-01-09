8f4b8b3ade13c2b7163fcd98a42bc721c732ad26
Leo Massaroli <massabash@DESKTOP-LBOHPPF.localdomain>
2022-01-09 12:23:09 -0300

    part 37

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export default () => {
-  return <div>Comment box</div>;
+  const [comment, setComment] = useState('');
+
+  const handleChange = (e) => {
+    setComment(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setComment('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>Add a Comment</h4>
+      <textarea value={comment} onChange={handleChange} />
+      <div>
+        <button>Submit Comment</button>
+      </div>
+    </form>
+  );
 };
