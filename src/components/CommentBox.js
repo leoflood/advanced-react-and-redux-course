5d233545dbdf37940bf3b9cea29170121c3da9fa
Leonardo Massaroli <leonardo.massaroli@corp.smtsoftware.com>
2022-01-20 13:47:46 -0300

    lesson 68

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -26,7 +26,9 @@ const CommentBox = ({ saveComment, fetchComments }) => {
           <button>Submit Comment</button>
         </div>
       </form>
-      <button onClick={fetchComments}>Fetch Comments</button>
+      <button className="fetch-comments" onClick={fetchComments}>
+        Fetch Comments
+      </button>
     </div>
   );
 };
