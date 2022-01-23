089b1b8b014b2dc3263d582ad8c0d9e5e2d0f077
Leonardo Massaroli <leonardo.massaroli@corp.smtsoftware.com>
2022-01-23 16:42:56 -0300

    lesson 87 middlewares

---
+++ src/middlewares/async.js
@@ -0,0 +1,20 @@
+export default ({ dispatch }) =>
+  (next) =>
+  (action) => {
+    // Check to see if the actions has a promise
+    // on its "payload" property
+    // If it does, then wait for it to resolve
+    // If it does not, then send the action on to
+    // the next middleware
+    if (!action.payload || !action.payload.then) {
+      return next(action);
+    }
+
+    // We want to wait for the promise to resolve
+    // (get its data!) and then create a new action
+    // with that data and dispatch it
+    action.payload.then((response) => {
+      const newAction = { ...action, payload: response };
+      dispatch(newAction);
+    });
+  };
